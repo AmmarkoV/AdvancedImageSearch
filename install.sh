8cf4338307a51d841eca9fb1f887d27d8709fc7e
Ammar Qammaz <ammarkov@gmail.com>
2013-02-24 13:15:05 +0200

    ....

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 echo "Copying to system bin directory"
 
-sudo mkdir /usr/lib/AdvancedImageSearch
+
+if [ -d "/usr/lib/AdvancedImageSearch" ]; then
+echo "AdvancedImageSearch Library path detected patching it up :)"
+else
+echo "Installing AdvancedImageSearch in the system.. :)"
+  sudo mkdir /usr/lib/AdvancedImageSearch
+fi
+
 sudo cp src/AdvancedImageSearchLib/libAdvancedImageSearchLib.so /usr/lib/AdvancedImageSearch/libAdvancedImageSearchLib.so
 
 ./makeSystem.sh
 
-sudo cp src/lsimg/lsimg /bin/lsimg 
+sudo cp src/lsimg/lsimg /usr/bin/lsimg 
 exit 0
