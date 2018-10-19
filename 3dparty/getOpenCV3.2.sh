e7fce1fb71e51438baa137ccf09571888d6f94d6
Ammar Qammaz <ammarkov@ics.forth.gr>
2018-10-19 16:29:01 +0300

    adding apt-get command to get all build requirements for openCV

--- 3dparty/getOpenCV3.2.sh
+++ 3dparty/getOpenCV3.2.sh
@@ -4,6 +4,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 
+sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev libtbb2 libtbb-dev
+#sudo apt-get install python3.5-dev python3-numpy  libjasper-dev
+sudo apt-get install build-essential libjpeg-dev libpng-dev libtiff5-dev  libdc1394-22-dev libeigen3-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev sphinx-common libtbb-dev yasm libfaac-dev libopencore-amrnb-dev libopencore-amrwb-dev libopenexr-dev libgstreamer-plugins-base1.0-dev libavutil-dev libavfilter-dev libavresample-dev
 
 #cd "$1"
 echo "Will Install @ `pwd`"
